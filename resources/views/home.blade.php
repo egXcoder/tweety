af2726b04b46ffe696e8174639ca8bb9245ae023
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-03-26 19:16:16 +0200

    initialize the home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,86 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <title>Tweety</title>
 
-                    You are logged in!
+    <!-- Fonts -->
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header mb-4">
+            <img src="https://static.wikia.nocookie.net/animaniacs/images/7/79/Tweety.png" class="img-fluid" width="80px">
+        </div>
+        <div class="row">
+            <section id="navigation" class="col-md-3">
+                <div class="d-flex flex-column">
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Home</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Explore</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Notifications</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Messages</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Bookmarks</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Lists</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">Profile</a>
+                    <a class="my-2 font-weight-bold text-black" style="color:black;" href="">More</a>
+                    <button class="btn btn-primary">Tweet-a-roo!</button>
                 </div>
-            </div>
+            </section>
+            <section id="main" class="col-md-6">
+                <form action="" method="post">
+                    <textarea name="content" class="form-control rounded" cols="30" rows="7">What's up doc?</textarea>
+                    <hr>
+                    <div>
+                        <img class="img-fluid float-left" width="50px"
+                            src="https://frspros.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg">
+                        <button type="button" class="btn btn-primary float-right">Tweet-a-roo!</button>
+                    </div>
+                </form>
+
+                <div class="posts">
+                    <div class="single-post row">
+                        <div class="col-md-2">
+                            <img class="img-fluid float-left" width="50px"
+                                src="https://frspros.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg">
+                        </div>
+                        <div class="col-md-10">
+                            <h4>Author Name <sh4an class="text-light">From Time</span> </p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, 
+                                    laboriosam! Ipsam, alias vel nobis mollitia, excepturi expedita iure tempora 
+                                    ullam natus quisquam nemo omnis. Corporis cumque laboriosam nobis sunt asperiores.
+                                </p>
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+            </section>
+            <section id="friends" class="col-md-3">
+                <div class="bg-light rounded">
+                    <h4>Friends</h4>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex my-1">
+                            <img class="img-fluid" width="50px" src="https://frspros.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg">
+                            <p class="my-auto mx-1">Ahmed Ibrahim</p>
+                        </div>
+                        <div class="d-flex my-1">
+                            <img class="img-fluid" width="50px"
+                                src="https://frspros.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg">
+                            <p class="my-auto mx-1">Ahmed Ibrahim</p>
+                        </div>
+                        <div class="d-flex my-1">
+                            <img class="img-fluid" width="50px"
+                                src="https://frspros.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg">
+                            <p class="my-auto mx-1">Ahmed Ibrahim</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
